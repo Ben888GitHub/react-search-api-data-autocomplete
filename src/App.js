1bb7920fd972c1fdc7f347d735830fec51a4730d
Benedict <benedictryan80@gmail.com>
2022-12-01 18:59:16 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { ReactSearchAutocomplete } from 'react-search-autocomplete';
 import './App.css';
+import ReactQueryFetch from './ReactQueryFetch';
+
+const items = [
+	{
+		id: 0,
+		name: 'Cobol'
+	},
+	{
+		id: 1,
+		name: 'JavaScript'
+	},
+	{
+		id: 2,
+		name: 'Basic'
+	},
+	{
+		id: 3,
+		name: 'PHP'
+	},
+	{
+		id: 4,
+		name: 'Java'
+	}
+];
+
+const formatResult = (item) => {
+	return (
+		<>
+			<span style={{ display: 'block', textAlign: 'left' }}>id: {item.id}</span>
+			<span style={{ display: 'block', textAlign: 'left' }}>
+				name: {item.name}
+			</span>
+		</>
+	);
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [searchMovies, setSearchMovies] = useState([]);
+	const [queryFilm, setQueryFilm] = useState('');
+
+	// const fetchMovies = async () => {
+	// 	const getMovies = await fetch(
+	// 		`https://api.themoviedb.org/3/search/multi?api_key=456ec94d71f9702ddcbbc1166b40f922&language=en-US&query=${queryFilm}`
+	// 	).then((r) => r.json());
+	// 	console.log(getMovies);
+	// 	setSearchMovies(getMovies);
+	// };
+
+	// useEffect(() => {
+	// 	queryFilm && fetchMovies();
+	// }, [queryFilm]);
+
+	return (
+		<div className="App">
+			<h1>Search Movie / TV Shows </h1>
+			<br />
+			{/* <div style={{ width: 400, margin: 'auto' }}>
+				<ReactSearchAutocomplete
+					items={searchMovies.results}
+					onSearch={(film) => setQueryFilm(film)}
+					// onHover={handleOnHover}
+					onSelect={(film) => console.log(film)}
+					// onFocus={handleOnFocus}
+					autoFocus
+					formatResult={formatResult}
+					maxResults={10}
+				/>
+			</div> */}
+			<ReactQueryFetch />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/ReactQueryFetch.js
@@ -0,0 +1,57 @@
+import { useState, Fragment } from 'react';
+import { ReactSearchAutocomplete } from 'react-search-autocomplete';
+import { useQuery } from '@tanstack/react-query';
+
+const formatResult = (item) => {
+	// console.log(item);
+
+	return (
+		<div style={{ cursor: 'pointer' }}>
+			<span style={{ display: 'block', textAlign: 'left' }}>id: {item.id}</span>
+			<span style={{ display: 'block', textAlign: 'left' }}>
+				name: {item.name || item.original_title}
+			</span>
+		</div>
+	);
+};
+
+function ReactQueryFetch() {
+	const [queryFilm, setQueryFilm] = useState('');
+	const [searchMovies, setSearchMovies] = useState([]);
+
+	const fetchFilm = async () => {
+		const movies = await fetch(
+			`https://api.themoviedb.org/3/search/multi?api_key=456ec94d71f9702ddcbbc1166b40f922&language=en-US&query=${queryFilm}`
+		).then((r) => r.json());
+		return movies;
+	};
+
+	const { data } = useQuery({
+		queryKey: ['films', queryFilm],
+		queryFn: () => queryFilm && fetchFilm(queryFilm),
+		enabled: Boolean(queryFilm), // this is to prevent auto-refetch
+		refetchOnWindowFocus: false,
+		refetchOnMount: false,
+		keepPreviousData: true
+	});
+
+	return (
+		<div>
+			<div style={{ width: 400, margin: 'auto' }}>
+				<ReactSearchAutocomplete
+					items={data?.results}
+					onSearch={(film) => setQueryFilm(film)}
+					// onHover={handleOnHover}
+					onSelect={(film) => console.log(film)}
+					// onFocus={handleOnFocus}
+					autoFocus
+					formatResult={formatResult}
+					maxResults={10}
+					placeholder="Search TV Shows or Movie"
+				/>
+			</div>
+		</div>
+	);
+}
+
+export default ReactQueryFetch;
